4d2270695e0a024744e08c08c6d2b05d36f7e2cc
xueshuai1 <578050550@qq.com>
2026-04-30 06:02:40 +0800

    auto: 集成ethics-010+blog-091到数据索引

--- src/data/blogs.ts
+++ src/data/blogs.ts
@@ -79,6 +79,7 @@ import { blog as metaManusGeopolitics } from './blogs/blog-087';
 import { blog as anthropicSurpassesOpenai } from './blogs/blog-088';
 import { blog as scoutAiMilitaryAutonomous } from './blogs/blog-089';
 import { blog as aiInfraStartupBoom } from './blogs/blog-090';
+import { blog as blog091 } from './blogs/blog-091';
 
 export const blogs: BlogPost[] = [
   blog059,
@@ -152,4 +153,5 @@ export const blogs: BlogPost[] = [
   anthropicSurpassesOpenai,
   scoutAiMilitaryAutonomous,
   aiInfraStartupBoom,
+  blog091,
 ];
